6176db0a1bd4e420e8b195b3df63fcd915c6e913
hseok <17452867+hs7982@users.noreply.github.com>
2024-08-22 23:18:30 +0900

    큰수의 법칙

--- Greedy/2.py
+++ Greedy/2.py
@@ -1,6 +1,8 @@
+#배열의 크기 N, 숫자가 더해지는 횟수 M, 연속으로 더해지는 횟수 상한 K
 n, m, k = map(int, input().split())
 data = list(map(int, input().split()))
 
+#데이터를 정렬시켜 가장큰수, 그다음으로 큰수 저장
 data.sort()
 first = data[n-1]
 second = data[n-2]
@@ -8,6 +10,7 @@ second = data[n-2]
 result = 0
 
 while True:
+    #큰수를 K만큼 더하고
     for i in range(k):
         if m == 0:
             break
@@ -15,6 +18,7 @@ while True:
         m -= 1
     if m == 0:
         break
+    #그다음으로 큰 수를 한번 더하는 방법 반복
     result += second
     m -= 1
 
