b4dc29c934dbfba9db33d7f53eb7115880cfaac0
hseok <17452867+hs7982@users.noreply.github.com>
2024-12-06 16:49:19 +0900

    2 add

--- IT응용/2.py
+++ IT응용/2.py
@@ -2,3 +2,8 @@
 #짝수 번째 원소들의 합 중 큰 값을 return 하도록 solution 함수를 완성하시오. 단, 두 값이 같을 경우 그
 #값을 return함.
 
+num_list = list(map(int, input().split(",")))
+odd = sum(num_list[::2])
+even = sum(num_list[1::2])
+
+print(max(odd, even))
