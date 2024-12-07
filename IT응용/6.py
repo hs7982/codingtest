204e7c3567a5639f504f99ee05ca8e47fb9d4d7c
hseok <17452867+hs7982@users.noreply.github.com>
2024-12-07 17:58:04 +0900

    문제 설명 주석 추가

--- IT응용/6.py
+++ IT응용/6.py
@@ -1,3 +1,7 @@
+# 2022 KAKAO BLIND RECRUITMENT에서 나온 기출문제
+# 이용자의 ID가 담긴 문자열 배열 id_list, 각 이용자가 신고한 이용자의 ID 정보가 담긴 문자열 배열
+# report, 정지 기준이 되는 신고 횟수 k가 매개변수로 주어질 때, 각 유저별로 처리 결과 메일을 받은 횟
+# 수를 배열에 담아 return 하도록 solution 함수를 완성해주세요.
 def solution(id_list, report, k):
     data = {}
     mailTo = {}
